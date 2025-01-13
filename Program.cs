bcc54aa5f048bccc100e7936343f8c5ee63c6e73
soulspine <soulspime@gmail.com>
2025-01-13 12:45:56 +0100

    add config and message routing system

--- Program.cs
+++ Program.cs
@@ -1,12 +1,95 @@
-﻿namespace LOBBYN.server
+﻿using System.Diagnostics;
+using Salaros.Configuration;
+using WebSocketSharp;
+using LOBBYN.ClassLibrary.Riot;
+
+namespace LOBBYN.server
 {
     internal class Program
     {
+        static public ConfigParser Config;
+        static public string ConfigFilePath;
+
         static void Main(string[] args)
         {
+            EnsureConfigFilled();
             Server.Start();
             Console.ReadKey();
             Server.Stop();
+        }
+
+        static void EnsureConfigFilled()
+        {
+            string appDataFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string dataFolderPath = Path.Combine(appDataFolderPath, "LOBBYN", "Server");
+            ConfigFilePath = Path.Combine(dataFolderPath, "config.ini");
+            if (!Directory.Exists(dataFolderPath))
+            {
+                Directory.CreateDirectory(dataFolderPath);
+                if (!File.Exists(ConfigFilePath))
+                {
+                    File.WriteAllText(ConfigFilePath, // default config
+@"[SERVER]
+PORT=8080
+
+[RIOT]
+API_KEY=YOUR_API_KEY
+CONTINENT=EUROPE
+");
+                }
+                Console.Write($"Looks like it's the first launch.\nPlease navigate to {ConfigFilePath} and fill in the config values.\nAfter doing so, press any key to validate them...");
+                Process.Start("notepad.exe", ConfigFilePath);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            
+            Config = new ConfigParser(ConfigFilePath);
+
+            bool valid = true;
+
+            if (Config["SERVER"]["PORT"].IsNullOrEmpty())
+            {
+                valid = false;
+                Console.WriteLine("[SERVER] PORT is required.");
+            }
+            else if (!int.TryParse(Config["SERVER"]["PORT"], out _))
+            {
+                valid = false;
+                Console.WriteLine("[SERVER] PORT has to be a number.");
+            }
+
+            if (Config["RIOT"]["API_KEY"].Length != 42)
+            {
+                valid = false;
+                Console.WriteLine("[RIOT] API_KEY has to be 42 characters long.");
+            }
+
+            // check if API KEY is not expired
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-Riot-Token", Config["RIOT"]["API_KEY"]);
+            HttpResponseMessage response = client.GetAsync("https://euw1.api.riotgames.com/lol/status/v4/platform-data").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                valid = false;
+                Console.WriteLine("[RIOT] API_KEY is invalid.");
+            }
+
+            if (!Enum.TryParse<Continent>(Config["RIOT"]["CONTINENT"], out _))
+            {
+                valid = false;
+                Console.Write("[RIOT] CONTINENT has to be one of the following: ");
+                foreach (Continent continent in Enum.GetValues(typeof(Continent)))
+                {
+                    Console.Write($"{continent.ToString()} ");
+                }
+                Console.WriteLine();
+            }
+
+            if (!valid)
+            {
+                Process.Start("notepad.exe", ConfigFilePath);
+                throw new ConfigParserException("Invalid config fields.");
+            }
         }
     }
 }
--- Server.cs
+++ Server.cs
@@ -1,4 +1,6 @@
-﻿using LOBBYN.server.DTOs;
+﻿using System.Reflection;
+using LOBBYN.ClassLibrary.Riot;
+using LOBBYN.ClassLibrary.DTOs;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using WebSocketSharp;
@@ -6,13 +8,6 @@ using WebSocketSharp.Server;
 
 namespace LOBBYN.server
 {
-    internal class AccountInfo
-    {
-        public string PlayerName { get; set; }
-        public string PlayerTagline { get; set; }
-        public PlayerRegion PlayerRegion { get; set; }
-    }
-
     internal class SocketClientInfo : AccountInfo
     {
         public string EncryptedPuuid { get; set; }
@@ -25,17 +20,33 @@ namespace LOBBYN.server
 
     internal class SocketBehavior : WebSocketBehavior
     {
-        private const string HEADER_WEBSOCKET_KEY = "Sec-WebSocket-Key";
+        private void Broadcast(string message)
+        {
+            foreach (KeyValuePair<string, SocketClientInfo> socket in Server.authorizedSockets)
+            {
+                if (socket.Key == ID) continue;
+                else Server.socketServer.WebSocketServices["/LOBBYN"].Sessions.SendTo(message, socket.Key);
+            }
+        }
+
+        private void SendByAccountInfo(string message, AccountInfo recipient)
+        {
+            var recipientSocket = Server.authorizedSockets.FirstOrDefault(socket => socket.Value.PlayerName == recipient.PlayerName && socket.Value.PlayerTagline == recipient.PlayerTagline);
+            if (recipientSocket.Value != null) Server.socketServer.WebSocketServices["/LOBBYN"].Sessions.SendTo(message, recipientSocket.Key);
+            else // check if recipient is registered and if yes, decide if to put it into a "mailbox"
+            {
+                // TODO
+            }
+        }
 
         protected override void OnOpen()
         {
-            string websocketKey = Context.Headers[HEADER_WEBSOCKET_KEY]!;
-            Console.WriteLine($"Connection {websocketKey} opened.");
-            Server.unauthorizedSockets.Add(websocketKey, null);
+            Console.WriteLine($"Connection {ID} opened.");
+            Server.unauthorizedSockets.Add(ID, null);
             Task.Run(async () =>
             {
                 await Task.Delay(30 * 1000); // 30 seconds
-                if (!Server.authorizedSockets.ContainsKey(websocketKey))
+                if (!Server.authorizedSockets.ContainsKey(ID))
                 {
                     Context.WebSocket.Close(CloseStatusCode.PolicyViolation, "Timed out");
                 }
@@ -44,15 +55,13 @@ namespace LOBBYN.server
 
         protected override void OnMessage(MessageEventArgs e)
         {
-            string websocketKey = Context.Headers[HEADER_WEBSOCKET_KEY]!;
-
-            if (Server.unauthorizedSockets.ContainsKey(websocketKey)) // still unauthorized
+            if (Server.unauthorizedSockets.ContainsKey(ID)) // still unauthorized
             {
                 if (e.Data == "Verify")
                 {
                     AuthInfo authInfo;
 
-                    if (Server.unauthorizedSockets.TryGetValue(websocketKey, out authInfo!))
+                    if (Server.unauthorizedSockets.TryGetValue(ID, out authInfo!))
                     {
                         HttpResponseMessage summonerResponse = Server.httpClient.GetAsync($"https://{authInfo.PlayerRegion}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{authInfo.EncryptedPuuid}").Result;
                         JObject summonerJson = JObject.Parse(summonerResponse.Content.ReadAsStringAsync().Result);
@@ -60,16 +69,16 @@ namespace LOBBYN.server
 
                         if (profileIconId == authInfo.IconId)
                         {
-                            Server.authorizedSockets.Add(websocketKey, new SocketClientInfo
+                            Server.authorizedSockets.Add(ID, new SocketClientInfo
                             {
                                 PlayerName = authInfo.PlayerName,
                                 PlayerTagline = authInfo.PlayerTagline,
                                 PlayerRegion = authInfo.PlayerRegion,
                                 EncryptedPuuid = authInfo.EncryptedPuuid
                             });
-                            Server.unauthorizedSockets.Remove(websocketKey);
+                            Server.unauthorizedSockets.Remove(ID);
                             Send("Verified");
-                            Console.WriteLine($"{websocketKey} verified as {authInfo.PlayerName}#{authInfo.PlayerTagline} {authInfo.PlayerRegion}");
+                            Console.WriteLine($"{ID} verified as {authInfo.PlayerName}#{authInfo.PlayerTagline} {authInfo.PlayerRegion}");
                             return;
                         }
                         else
@@ -79,7 +88,7 @@ namespace LOBBYN.server
                         }
                     }
                 }
-                else if (Server.unauthorizedSockets[websocketKey] == null)
+                else if (Server.unauthorizedSockets[ID] == null)
                 {
                     AuthInfo authInfo;
                     try
@@ -98,7 +107,7 @@ namespace LOBBYN.server
                         return;
                     }
 
-                    HttpResponseMessage accountResponse = Server.httpClient.GetAsync($"https://{Server.RIOT_CONTINENT}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{authInfo.PlayerName}/{authInfo.PlayerTagline}").Result;
+                    HttpResponseMessage accountResponse = Server.httpClient.GetAsync($"https://{Server.continent}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{authInfo.PlayerName}/{authInfo.PlayerTagline}").Result;
 
                     if (!accountResponse.IsSuccessStatusCode)
                     {
@@ -128,62 +137,97 @@ namespace LOBBYN.server
                         authInfo.IconId = random.Next(0, 30);
                     }
 
-                    Server.unauthorizedSockets[websocketKey] = authInfo;
-                    Send(Server.unauthorizedSockets[websocketKey]!.IconId.ToString());
-                    Console.WriteLine($"{websocketKey} introduced themselves as {authInfo.PlayerName}#{authInfo.PlayerTagline} {authInfo.PlayerRegion}");
+                    Server.unauthorizedSockets[ID] = authInfo;
+                    Send(Server.unauthorizedSockets[ID]!.IconId.ToString());
+                    Console.WriteLine($"{ID} introduced themselves as {authInfo.PlayerName}#{authInfo.PlayerTagline} {authInfo.PlayerRegion}");
                     return;
                 }
             }
 
             // close connection if not verified
-            if (!Server.authorizedSockets.ContainsKey(websocketKey))
+            if (!Server.authorizedSockets.ContainsKey(ID))
             {
                 Context.WebSocket.Close(CloseStatusCode.PolicyViolation, "Unauthorized");
                 return;
+            }
+
+            // handle messages
+            ClientToServerMessage receivedMessage;
+            try
+            {
+                receivedMessage = JsonConvert.DeserializeObject<ClientToServerMessage>(e.Data)!;
+            }
+            catch (JsonException)
+            {
+                Context.WebSocket.Close(CloseStatusCode.PolicyViolation, "Invalid JSON");
+                return;
+            }
+
+            ServerToClientMessage outgoingMessage = new ServerToClientMessage
+            {
+                SenderName = Server.authorizedSockets[ID].PlayerName,
+                SenderTagling = Server.authorizedSockets[ID].PlayerTagline,
+                Type = receivedMessage.MessageType,
+                Data = receivedMessage.Data
+            };
+
+            string outgoingMessageString = JsonConvert.SerializeObject(outgoingMessage);
+
+            switch (receivedMessage.RoutingType)
+            {
+                case MessageRoutingType.Broadcast:
+                    Broadcast(outgoingMessageString);
+                    break;
+
+                case MessageRoutingType.Direct:
+                    foreach (var recepient in receivedMessage.Recipients)
+                    {
+                        SendByAccountInfo(outgoingMessageString, recepient);
+                    }
+                    break;
+                default:
+                    Context.WebSocket.Close(CloseStatusCode.PolicyViolation, "Invalid Message Routing Type");
+                    break;
             }
         }
 
         protected override void OnClose(CloseEventArgs e)
         {
-            string websocketKey = Context.Headers[HEADER_WEBSOCKET_KEY]!;
-            Console.WriteLine($"Connection {websocketKey} closed - {e.Code} {e.Reason}");
+            Console.WriteLine($"Connection {ID} closed - {e.Code} {e.Reason}");
 
-            if (Server.unauthorizedSockets.ContainsKey(websocketKey)) Server.unauthorizedSockets.Remove(websocketKey);
-            else if (Server.authorizedSockets.ContainsKey(websocketKey)) Server.authorizedSockets.Remove(websocketKey);
+            if (Server.unauthorizedSockets.ContainsKey(ID)) Server.unauthorizedSockets.Remove(ID);
+            else if (Server.authorizedSockets.ContainsKey(ID)) Server.authorizedSockets.Remove(ID);
         }
     }
     public static class Server
     {
-        internal const string RIOT_CONTINENT = "europe";
         internal static readonly HttpClient httpClient = new HttpClient();
         internal static Dictionary<string, AuthInfo?> unauthorizedSockets = new Dictionary<string, AuthInfo?>();
         internal static Dictionary<string, SocketClientInfo> authorizedSockets = new Dictionary<string, SocketClientInfo>();
-        private static WebSocketServer socketServer = new WebSocketServer($"ws://127.0.0.1:{PORT}");
-        private const int PORT = 8080;
+        internal static WebSocketServer socketServer;
+        internal static int port;
+        internal static string continent;
         public static bool Running => socketServer.IsListening;
 
         static Server()
         {
+            port = Convert.ToInt32(Program.Config["SERVER"]["PORT"]);
+            continent = Program.Config["RIOT"]["CONTINENT"];
+            socketServer = new WebSocketServer($"ws://127.0.0.1:{port}");
             socketServer.AddWebSocketService<SocketBehavior>("/LOBBYN");
-            httpClient.DefaultRequestHeaders.Add("X-Riot-Token", File.ReadAllText("RIOT_API_KEY"));
+            httpClient.DefaultRequestHeaders.Add("X-Riot-Token", Program.Config["RIOT"]["API_KEY"]);
         }
 
         public static void Start()
         {
             socketServer.Start();
-            Console.WriteLine($"Server started on port {PORT}");
+            Console.WriteLine($"Server started on port {port}");
         }
 
         public static void Stop()
         {
             socketServer.Stop();
             Console.WriteLine("Server stopped");
-        }
-
-        internal static JObject RiotApiRequest(string url)
-        {
-            HttpResponseMessage response = Server.httpClient.GetAsync(url).Result;
-            return JObject.Parse(response.Content.ReadAsStringAsync().Result);
         }
     }
 }
